da12dd26e0ffb9c7997fc526f33a35886be631cb
vagrant <vagrant@homestead>
2015-08-27 08:17:47 +0000

    initial commit

---
+++ resources/views/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <base href='/'>
+    <title>Wine-Site</title>
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+</head>
+<body>
+<h1>Hello World!!!</h1>
+<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="bower_components/angular/angular.min.js"></script>
+<script type="text/javascript" src="bower_components/angular-ui-router/release/angular-ui-router.min.js"></script>
+<script type="text/javascript" src="bower_components/angular-bootstrap/ui-bootstrap.min.js"></script>
+<script type="text/javascript" src="bower_components/angular-sanitize/angular-sanitize.min.js"></script>
+<script type="text/javascript" src="bower_components/satellizer/satellizer.min.js"></script>
+</body>
+</html>
+
